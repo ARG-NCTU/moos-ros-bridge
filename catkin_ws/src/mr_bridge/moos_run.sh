d28afa08a58cc3347fb47348ca9135d736d60f31
hsuchaochun <leohsu210200@gmail.com>
2021-04-22 12:37:56 +0800

    change pkg name

--- catkin_ws/src/mr_bridge/moos_run.sh
+++ catkin_ws/src/mr_bridge/moos_run.sh
@@ -1,2 +1,2 @@
-cd moosros/moos/BH_cave
+cd bridge/m_launch/BH_cave
 pAntler test.moos
--- catkin_ws/src/mr_bridge/tester/scripts/example.sh
+++ catkin_ws/src/mr_bridge/tester/scripts/example.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Launch the ROS nodes
-roslaunch moosros_tester examle.launch &
+roslaunch tester examle.launch &
 ROSLAUNCH_PID=$!
 
 # Launch the MOOS nodes
-pAntler $(rospack find moosros)/moos/example/bridge.moos >& /dev/null &
+pAntler $(rospack find bridge)/m_launch/example/bridge.moos >& /dev/null &
 PANTLER_PID=$!
 
-uMAC $(rospack find moosros)/moos/example/bridge.moos
+uMAC $(rospack find bridge)/moos_launch/example/bridge.moos
 
 printf "Killing all processes, safely ... \n"
 kill -SIGTERM $ROSLAUNCH_PID
